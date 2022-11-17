9960e2e45cb6a3a081193b6fa86ef6bbe1a371a3
Auryn Macmillan <hi@auryn.xyz>
2022-11-16 22:26:59 -0500

    fix token overrides

--- contracts/ERC20Votes.sol
+++ contracts/ERC20Votes.sol
@@ -1,84 +1,84 @@
-// // SPDX-License-Identifier: LGPL-3.0-only
-// pragma solidity ^0.8.9;
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.9;
 
-// import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-// contract ERC20Votes is
-//     Initializable,
-//     ERC20Upgradeable,
-//     ERC20BurnableUpgradeable,
-//     PausableUpgradeable,
-//     OwnableUpgradeable,
-//     ERC20PermitUpgradeable,
-//     ERC20VotesUpgradeable
-// {
-//     constructor(
-//         address _owner,
-//         string memory _name,
-//         string memory _symbol
-//     ) {
-//         bytes memory initializeParams = abi.encode(_owner, _name, _symbol);
-//         setUp(initializeParams);
-//     }
+contract ERC20Votes is
+    Initializable,
+    ERC20Upgradeable,
+    ERC20BurnableUpgradeable,
+    PausableUpgradeable,
+    OwnableUpgradeable,
+    ERC20PermitUpgradeable,
+    ERC20VotesUpgradeable
+{
+    constructor(
+        address _owner,
+        string memory _name,
+        string memory _symbol
+    ) {
+        bytes memory initializeParams = abi.encode(_owner, _name, _symbol);
+        setUp(initializeParams);
+    }
 
-//     /// @dev Initialize function, will be triggered when a new proxy is deployed
-//     /// @param initializeParams Parameters of initialization encoded
-//     function setUp(bytes memory initializeParams) public initializer {
-//         (address _owner, string memory _name, string memory _symbol) = abi.decode(
-//             initializeParams,
-//             (address, string, string)
-//         );
+    /// @dev Initialize function, will be triggered when a new proxy is deployed
+    /// @param initializeParams Parameters of initialization encoded
+    function setUp(bytes memory initializeParams) public initializer {
+        (address _owner, string memory _name, string memory _symbol) = abi.decode(
+            initializeParams,
+            (address, string, string)
+        );
 
-//         __ERC20_init(_name, _symbol);
-//         __ERC20Burnable_init();
-//         __Pausable_init();
-//         __Ownable_init();
-//         __ERC20Permit_init(_name);
-//         __ERC20Votes_init();
-//         transferOwnership(_owner);
-//     }
+        __ERC20_init(_name, _symbol);
+        __ERC20Burnable_init();
+        __Pausable_init();
+        __Ownable_init();
+        __ERC20Permit_init(_name);
+        __ERC20Votes_init();
+        transferOwnership(_owner);
+    }
 
-//     function pause() public onlyOwner {
-//         _pause();
-//     }
+    function pause() public onlyOwner {
+        _pause();
+    }
 
-//     function unpause() public onlyOwner {
-//         _unpause();
-//     }
+    function unpause() public onlyOwner {
+        _unpause();
+    }
 
-//     function mint(address to, uint256 amount) public onlyOwner {
-//         _mint(to, amount);
-//     }
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 
-//     function _beforeTokenTransfer(
-//         address from,
-//         address to,
-//         uint256 amount
-//     ) internal override whenNotPaused {
-//         super._beforeTokenTransfer(from, to, amount);
-//     }
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
+    }
 
-//     // The following functions are overrides required by Solidity.
+    // The following functions are overrides required by Solidity.
 
-//     function _afterTokenTransfer(
-//         address from,
-//         address to,
-//         uint256 amount
-//     ) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
-//         super._afterTokenTransfer(from, to, amount);
-//     }
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
+        super._afterTokenTransfer(from, to, amount);
+    }
 
-//     function _mint(address to, uint256 amount) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
-//         super._mint(to, amount);
-//     }
+    function _mint(address to, uint256 amount) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
+        super._mint(to, amount);
+    }
 
-//     function _burn(address account, uint256 amount) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
-//         super._burn(account, amount);
-//     }
-// }
+    function _burn(address account, uint256 amount) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
+        super._burn(account, amount);
+    }
+}
--- contracts/ERC721Votes.sol
+++ contracts/ERC721Votes.sol
@@ -1,95 +1,94 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.4;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-// import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/draft-ERC721VotesUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-// import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/draft-ERC721VotesUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
-// contract ERC721Votes is
-//     Initializable,
-//     ERC721Upgradeable,
-//     ERC721EnumerableUpgradeable,
-//     PausableUpgradeable,
-//     OwnableUpgradeable,
-//     ERC721BurnableUpgradeable,
-//     EIP712Upgradeable,
-//     ERC721VotesUpgradeable
-// {
-//     using CountersUpgradeable for CountersUpgradeable.Counter;
+contract ERC721Votes is
+    Initializable,
+    ERC721Upgradeable,
+    ERC721EnumerableUpgradeable,
+    PausableUpgradeable,
+    OwnableUpgradeable,
+    ERC721BurnableUpgradeable,
+    EIP712Upgradeable,
+    ERC721VotesUpgradeable
+{
+    using CountersUpgradeable for CountersUpgradeable.Counter;
 
-//     CountersUpgradeable.Counter private _tokenIdCounter;
+    CountersUpgradeable.Counter private _tokenIdCounter;
 
-//     constructor(
-//         address _owner,
-//         string memory _name,
-//         string memory _symbol
-//     ) {
-//         bytes memory initializeParams = abi.encode(_owner, _name, _symbol);
-//         setUp(initializeParams);
-//     }
+    constructor(
+        address _owner,
+        string memory _name,
+        string memory _symbol
+    ) {
+        bytes memory initializeParams = abi.encode(_owner, _name, _symbol);
+        setUp(initializeParams);
+    }
 
-//     /// @dev Initialize function, will be triggered when a new proxy is deployed
-//     /// @param initializeParams Parameters of initialization encoded
-//     function setUp(bytes memory initializeParams) public initializer {
-//         (address _owner, string memory _name, string memory _symbol) = abi.decode(
-//             initializeParams,
-//             (address, string, string)
-//         );
-//         __ERC721_init(_name, _symbol);
-//         __ERC721Enumerable_init();
-//         __Pausable_init();
-//         __Ownable_init();
-//         __ERC721Burnable_init();
-//         __EIP712_init(_name, "1");
-//         __ERC721Votes_init();
+    /// @dev Initialize function, will be triggered when a new proxy is deployed
+    /// @param initializeParams Parameters of initialization encoded
+    function setUp(bytes memory initializeParams) public initializer {
+        (address _owner, string memory _name, string memory _symbol) = abi.decode(
+            initializeParams,
+            (address, string, string)
+        );
+        __ERC721_init(_name, _symbol);
+        __ERC721Enumerable_init();
+        __Pausable_init();
+        __Ownable_init();
+        __ERC721Burnable_init();
+        __EIP712_init(_name, "1");
+        __ERC721Votes_init();
 
-//         transferOwnership(_owner);
-//     }
+        transferOwnership(_owner);
+    }
 
-//     function pause() public onlyOwner {
-//         _pause();
-//     }
+    function pause() public onlyOwner {
+        _pause();
+    }
 
-//     function unpause() public onlyOwner {
-//         _unpause();
-//     }
+    function unpause() public onlyOwner {
+        _unpause();
+    }
 
-//     function safeMint(address to) public onlyOwner {
-//         uint256 tokenId = _tokenIdCounter.current();
-//         _tokenIdCounter.increment();
-//         _safeMint(to, tokenId);
-//     }
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
 
-//     function _beforeTokenTransfer(
-//         address from,
-//         address to,
-//         uint256 tokenId
-//     ) internal override(ERC721Upgradeable, ERC721EnumerableUpgradeable) whenNotPaused {
-//         super._beforeTokenTransfer(from, to, tokenId);
-//     }
+  function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        whenNotPaused
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
 
-//     // The following functions are overrides required by Solidity.
+    // The following functions are overrides required by Solidity.
 
-//     function _afterTokenTransfer(
-//         address from,
-//         address to,
-//         uint256 tokenId
-//     ) internal override(ERC721Upgradeable, ERC721VotesUpgradeable) {
-//         super._afterTokenTransfer(from, to, tokenId);
-//     }
+    function _afterTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721Upgradeable, ERC721VotesUpgradeable)
+    {
+        super._afterTokenTransfer(from, to, tokenId, batchSize);
+    }
 
-//     function supportsInterface(bytes4 interfaceId)
-//         public
-//         view
-//         override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
-//         returns (bool)
-//     {
-//         return super.supportsInterface(interfaceId);
-//     }
-// }
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
