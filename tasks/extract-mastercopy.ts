ab083e1a0fc00aad1679bb5b95aac8638814bde0
juliopavila <javila1997@gmail.com>
2024-09-05 14:12:43 -0300

    feat: refactor extract mastercopy

--- tasks/extract-mastercopy.ts
+++ tasks/extract-mastercopy.ts
@@ -1,4 +1,5 @@
 import { task } from "hardhat/config"
+import { ethers, ZeroHash } from "ethers"
 
 import { writeMastercopyFromBuild } from "@gnosis-guild/zodiac-core"
 
@@ -15,7 +16,70 @@ task("extract:mastercopy", "Extracts and persists current mastercopy build artif
     }),
     constructorArgs: {
       types: ["address", "string", "string"],
-      values: [AddressOne, "", ""],
+      values: [AddressOne, "0x", "0x"],
+    },
+    salt: ZeroHash,
+  })
+  writeMastercopyFromBuild({
+    contractVersion: packageJson.version,
+    contractName: "ERC721Votes",
+    compilerInput: await hre.run("verify:etherscan-get-minimal-input", {
+      sourceName: "contracts/ERC721Votes.sol",
+    }),
+    constructorArgs: {
+      types: ["address", "string", "string"],
+      values: [AddressOne, "0x", "0x"],
+    },
+    salt: ZeroHash,
+  })
+  writeMastercopyFromBuild({
+    contractVersion: packageJson.version,
+    contractName: "MultisendEncoder",
+    compilerInput: await hre.run("verify:etherscan-get-minimal-input", {
+      sourceName: "contracts/MultisendEncoder.sol",
+    }),
+    constructorArgs: {
+      types: [],
+      values: [],
+    },
+    salt: "0x0000000000000000000000000000000000000000000000000000000000000000",
+  })
+
+  await hre.run("verify:etherscan-get-minimal-input", {
+    sourceName: "contracts/OZGovernorModule.sol",
+  })
+
+  writeMastercopyFromBuild({
+    contractVersion: packageJson.version,
+    contractName: "OZGovernorModule",
+    compilerInput: await hre.run("verify:etherscan-get-minimal-input", {
+      sourceName: "contracts/OZGovernorModule.sol",
+    }),
+    constructorArgs: {
+      types: [
+        "address",
+        "address",
+        "address",
+        "address",
+        "string",
+        "uint256",
+        "uint256",
+        "uint256",
+        "uint256",
+        "uint64",
+      ],
+      values: [
+        "0x0000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000001",
+        "",
+        "0x0000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000001",
+      ],
     },
     salt: "0x0000000000000000000000000000000000000000000000000000000000000000",
   })
